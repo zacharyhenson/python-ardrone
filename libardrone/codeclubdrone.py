326f7e6ed00b53aa51238d1d82ad280c50483ccc
Adrian Taylor <adrian@bromium.com>
2014-06-15 15:24:57 +0100

    Adding exercise.py to be expanded in exercises.

---
+++ libardrone/codeclubdrone.py
@@ -0,0 +1,105 @@
+__author__ = 'adrian'
+
+import pygame
+import pygame.surfarray
+
+import pygame.transform
+import time
+import libardrone
+import libfakeardrone
+from threading import Thread
+
+drone = None
+FAKE = True
+running = True
+keys = dict()
+screen = None
+clock = None
+
+
+def setup_drone():
+    global screen, drone, clock
+    pygame.init()
+    W, H = 640, 480
+    screen = pygame.display.set_mode((W, H))
+    if FAKE:
+        drone = libfakeardrone.ARDrone(True)
+    else:
+        drone = libardrone.ARDrone(True)
+    drone.reset()
+    clock = pygame.time.Clock()
+    time.sleep(2.0)
+    drone.trim()
+
+
+def get_drone():
+    if drone is None:
+        raise Exception("Please call setup_drone() first")
+    return drone
+
+
+def bind_key(key, function):
+    keys[key] = function
+
+
+def bind_common_keys():
+    bind_key(pygame.K_ESCAPE, lambda: get_drone().reset())
+    bind_key(pygame.K_RETURN, lambda: get_drone().takeoff())
+    bind_key(pygame.K_SPACE, lambda: get_drone().takeoff())
+    bind_key(pygame.K_BACKSPACE, lambda: get_drone().reset())
+    bind_key(pygame.K_w, lambda: get_drone().move_forward())
+    bind_key(pygame.K_s, lambda: get_drone().move_backward())
+    bind_key(pygame.K_a, lambda: get_drone().move_left())
+    bind_key(pygame.K_d, lambda: get_drone().move_right())
+    bind_key(pygame.K_UP, lambda: get_drone().move_up())
+    bind_key(pygame.K_DOWN, lambda: get_drone().move_down())
+    bind_key(pygame.K_LEFT, lambda: get_drone().turn_left())
+    bind_key(pygame.K_RIGHT, lambda: get_drone().turn_right())
+
+    def set_speed(speed):
+        get_drone().speed = speed
+
+    bind_key(pygame.K_0, lambda: set_speed(1.0))
+    for i in range(9):
+        bind_key(pygame.K_1 + i, lambda: set_speed(i/10.0))
+
+
+def start_running_drone():
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                stop()
+            elif event.type == pygame.KEYUP:
+                drone.hover()
+            elif event.type == pygame.KEYDOWN:
+                if event.key in keys:
+                    action = keys[event.key]
+                    t = Thread(target=action)
+                    t.daemon = True
+                    t.start()
+        try:
+            # print pygame.image
+            pixelarray = drone.get_image()
+            if pixelarray is not None:
+                surface = pygame.surfarray.make_surface(pixelarray)
+                rotsurface = pygame.transform.rotate(surface, 270)
+                screen.blit(rotsurface, (0, 0))
+            # battery status
+            hud_color = (255, 0, 0) if drone.navdata.get('drone_state', dict()).get('emergency_mask', 1) else (10, 10, 255)
+            bat = drone.navdata.get(0, dict()).get('battery', 0)
+            f = pygame.font.Font(None, 20)
+            hud = f.render('Battery: %i%%' % bat, True, hud_color)
+            screen.blit(hud, (10, 10))
+        except:
+            pass
+
+        pygame.display.flip()
+        clock.tick(50)
+        pygame.display.set_caption("FPS: %.2f" % clock.get_fps())
+
+    print("Shutting down...")
+    drone.halt()
+    print("Ok.")
+
+def stop():
+    running = False
---
+++ libardrone/exercise.py
@@ -0,0 +1,17 @@
+
+from codeclubdrone import *
+from pygame import *
+
+setup_drone()
+drone = get_drone()
+bind_common_keys()
+
+def fly_around():
+    drone.takeoff()
+    drone.land()
+
+# The following means that whenever you press 'a',
+# your function 'fly_around' above will get run.
+bind_key(K_x, fly_around)
+
+start_running_drone()
